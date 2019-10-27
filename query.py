7290855feb40c31fd7badd6feceed0ae2b60b17c
asaflevi1 <asaf.levi.gever.master@gmail.com>
2019-10-27 15:51:26 +0200

    Changed query.py

--- query.py
+++ query.py
@@ -1,19 +1,6 @@
-'''
-Pattern Structure is a list of qitems
-Pattern Matching Condition is a formula
-Sliding Window's format is to be discussed.
-'''
 class Query:
-    def __init__(self, patternStructure, patternMatchingCondition, slidingWindow):
-        self.__patternStructure = patternStructure
-        self.__patternMatchingCondition = patternMatchingCondition
-        self.__slidingWindow = slidingWindow
+    def __init__(self, query):
+        self.__query = query
 
-    def getPatternStructure():
-        return self.__patternStructure
-        
-    def getPatternMatchingCondition():
-        return self.__patternMatchingCondition
-        
-    def getSlidingWIndow():
-        return self.__slidingWindow
+    def getQuery(self):
+        return self.__query
