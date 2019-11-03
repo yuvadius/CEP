fe71356fe5dac83a1a9b34d4cac17e779d8ff184
yuvadius <yuvalgliz@gmail.com>
2019-11-03 13:44:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,4 @@ EqFormula(IdentifierTerm("s", lambda x: x[0]), AtomicTerm("AAPL"))
 )
 
 cep = CEP(events)
-result = cep.findPattern(query, Tree, False)
-
-print(result)
+cep.findPattern(query, Tree, False)
